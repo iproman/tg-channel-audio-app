079dced37a5d3249f9d6aea0e979384ab55c0973
iproman <itechman@yandex.ru>
2020-08-23 17:09:39 +0500

    Change method name.

--- index.py
+++ index.py
@@ -178,7 +178,7 @@ class Ui_MainWindow(object):
 
         # Buttons
         self.prepareBtn.clicked.connect(self.convert)
-        self.clearBtn.clicked.connect(self.clear)
+        self.clearBtn.clicked.connect(self.clear_inputs)
         self.trackerBtn.clicked.connect(self.parse_data)
 
         # MenuBar, statusBar
@@ -255,7 +255,7 @@ class Ui_MainWindow(object):
         text_file.close()
 
     # Clear inputs
-    def clear(self):
+    def clear_inputs(self):
         self.nameEdit.clear()
         self.authorEdit.clear()
         self.readEdit.clear()
